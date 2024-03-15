ec7340459145a45d814f3b3cf6cbc3e2951c79a8
Seção Tec Inf FCF <stifcf@usp.br>
2024-03-15 10:32:44 -0300

    Initial Commit

--- src/AssinaturaServiceProvider.php
+++ src/AssinaturaServiceProvider.php
@@ -7,17 +7,17 @@ use Illuminate\Support\ServiceProvider;
 class AssinaturaServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/config/assinatura.php', 'assinatura'
-        );
-
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views','assinatura');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
+        /*
+        $this->mergeConfigFrom(
+            __DIR__.'/config/assinatura.php', 'assinatura'
+        );
         $this->publishes([
             __DIR__.'/config/assinatura.php' => config_path('assinatura.php')
-        ], 'assinatura-config');
+        ], 'assinatura-config');*/
         
     }
     
